107dac1562926aacbb1748a6ee2acc8de7615a90
sarassoroberto <sarassoroberto@gmail.com>
2019-04-11 23:24:02 +0200

    Prima versione della classe di routing - aggiunti file di configurazione

--- gestione_video_php/config.php
+++ gestione_video_php/config.php
@@ -3,3 +3,9 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'gestione_video');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
+
+class Config
+{
+    const DEFAULT_CONTROLLER = 'gestione_utenti';
+    const DEFAULT_ACTION = 'tutti';
+}
--- gestione_video_php/index.php
+++ gestione_video_php/index.php
@@ -3,29 +3,6 @@ require './config.php';
 require './__autoload.php';
 require './vendor/autoload.php';
 
-error_reporting(E_ALL & ~E_NOTICE);
-ini_set('display_errors', 1);
 //index.php?controller=gestione_utenti&action=tutti
 
-/**
- * gestione_utenti
- *  - visualizzotutti
- *  - visualizzo solo quelli attivi
- */
-
-//error_reporting(E_ALL);
-//gestione_utenti
-
-$controller = filter_input(INPUT_GET, 'controller');
-//tutti
-$action = filter_input(INPUT_GET, 'action');
-
-$controllerName = ucfirst($controller) . '_controller';
-
-//echo $controllerName . "<br>";
-/**
- * @todo: definire un controller predefinito, o impostare la risposta http con codice 404
- */
-
-$controller_attivo = new $controllerName();
-$controller_attivo->$action();
+Router::getRoute();
