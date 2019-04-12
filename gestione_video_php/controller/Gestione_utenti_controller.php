92c8fad1c078da31aa18011be4a373c5a1108d64
roberto sarasso <sarassoroberto@gmail.com>
2019-04-12 18:00:15 +0200

    Tabella utenti - eliminazione  non funziona e tpl da finire

--- gestione_video_php/controller/Gestione_utenti_controller.php
+++ gestione_video_php/controller/Gestione_utenti_controller.php
@@ -18,7 +18,24 @@ class Gestione_utenti_controller
         $loader = new \Twig\Loader\FilesystemLoader('./view');
         $twig = new \Twig\Environment($loader);
 
-        echo $twig->render('gestione_utenti/tutti_gli_utenti.html', ['autori' => $res]);
+        echo $twig->render(
+            'gestione_utenti/tutti_gli_utenti.html',
+            ['autori' => $res]
+        );
         //print_r($twig);
+    }
+
+    public function eliminaAutore()
+    {
+        $id_autore = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+        echo "elimina autore $id_autore";
+
+        if ($id_autore) {
+            $dao = new AutoreDao();
+            $dao->delete($id_autore);
+        }
+        // "gestione_utenti/tutti"
+        $url = "index.php?controller=gestione_utenti&action=tutti";
+        //header("Location: $url");
     }
 }
