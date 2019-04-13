f8b9980fc3e46350355748556c793f3ed99d0a01
roberto.sarasso <sarassoroberto@gmail.com>
2019-04-13 07:09:09 +0200

    Venerdi 12 Aprile 2019 - impostati i getter e i setter mancanti nota: Se non vengono compilati i getter e i setter dei value object (utenti,video) non si  riesce ad accedere alle proprietà

--- gestione_video_php/class/Autore.php
+++ gestione_video_php/class/Autore.php
@@ -52,6 +52,96 @@ class Autore implements \JsonSerializable
     }
 
 
+    
+
+    /**
+     * Get the value of cognome
+     */ 
+    public function getCognome()
+    {
+        return $this->cognome;
+    }
+
+    /**
+     * Set the value of cognome
+     *
+     * @return  self
+     */ 
+    public function setCognome($cognome)
+    {
+        $this->cognome = $cognome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of data_nascita
+     */ 
+    public function getData_nascita()
+    {
+        return $this->data_nascita;
+    }
+
+    /**
+     * Set the value of data_nascita
+     *
+     * @return  self
+     */ 
+    public function setData_nascita($data_nascita)
+    {
+        $this->data_nascita = $data_nascita;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of avatar
+     */ 
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * Set the value of avatar
+     *
+     * @return  self
+     */ 
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id_autore
+     */ 
+    public function getId_autore()
+    {
+        return $this->id_autore;
+    }
+
     public function  jsonSerialize()
     {
 
