b618954b33c7240ae0063475b23781cc672d294c
roberto sarasso <sarassoroberto@gmail.com>
2019-04-15 17:34:53 +0200

    [eliminazione utenti] - implementata eliminazoine multipla

--- gestione_video_php/controller/Gestione_utenti_controller.php
+++ gestione_video_php/controller/Gestione_utenti_controller.php
@@ -42,4 +42,31 @@ class Gestione_utenti_controller
         $url = RouterCore::link('gestione_utenti', 'tutti');
         header("Location: $url");
     }
+
+    public function eliminaUtenti()
+    {
+        echo __FUNCTION__ . " ";
+        // $ids_autore = filter_input(INPUT_POST, 'id_autore', null, array(
+
+        //     'flags'  => FILTER_REQUIRE_ARRAY
+        // ));
+
+        $ids_autore = filter_input(
+            INPUT_POST,
+            'id_autore',
+            FILTER_DEFAULT,
+            FILTER_REQUIRE_ARRAY
+        );
+
+        $dao = new AutoreDao();
+        foreach ($ids_autore as $id_autore) {
+            $dao->delete($id_autore);
+        }
+
+        $url = RouterCore::link('gestione_utenti', 'tutti');
+        header("Location: $url");
+
+        ///print_r($_POST['id_autore']);
+        //print_r($ids_autore);
+    }
 }
